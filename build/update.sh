d1f72d77e4679e9bb9f5d60bb2879112fa5f914f
hz9527 <981379316@qq.com>
2017-11-26 11:06:10 +0800

    test

--- build/update.sh
+++ build/update.sh
@@ -4,15 +4,18 @@ if [[ $REPLY =~ ^[Yy]$ ]];then
   gitbook build
   git checkout gh-pages
   if [[ $? == 0 ]];then
-    mv ./_book/books ./_book/gitbook ./_book/index.html ./_book/methods.md ./search_index.json ./
+    rm -rf ./books ./gitbook
+    rm -f ./index.html ./methods.md ./search_index.json
+    mv ./_book/books ./_book/gitbook ./_book/index.html ./_book/methods.md ./_book/search_index.json ./
     git status
     git add .
     git commit -m ":memo: update notes"
     git push origin gh-pages
-    # if [[ $? == 0 ]];then
-    #   git checkout master
-    # else
-    #   echo "some error in gh-pages"
-    # fi
+    if [[ $? == 0 ]];then
+      git checkout master
+      gitbook serve .
+    else
+      echo "some error in gh-pages"
+    fi
   fi
 fi
