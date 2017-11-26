b96f89f4c682dfa1f354ca2c6a5479902699b62e
hz9527 <981379316@qq.com>
2017-11-26 11:26:01 +0800

    :bug: fix sh

--- build/update.sh
+++ build/update.sh
@@ -4,9 +4,10 @@ if [[ $REPLY =~ ^[Yy]$ ]];then
   gitbook build
   git checkout gh-pages
   if [[ $? == 0 ]];then
+    git pull origin gh-pages
     rm -rf ./books ./gitbook
-    rm -f ./index.html ./methods.md ./search_index.json
-    mv ./_book/books ./_book/gitbook ./_book/index.html ./_book/methods.md ./_book/search_index.json ./
+    rm -f ./index.html ./search_index.json
+    mv ./_book/books ./_book/gitbook ./_book/index.html  ./_book/search_index.json ./
     git status
     git add .
     git commit -m ":memo: update notes"
