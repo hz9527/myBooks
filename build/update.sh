18c4967364c67c78964d8ce05d14538962ff7eb2
hz9527 <981379316@qq.com>
2017-11-26 10:34:06 +0800

    test

--- build/update.sh
+++ build/update.sh
@@ -4,8 +4,9 @@ if [[ $REPLY =~ ^[Yy]$ ]];then
   gitbook build
   git checkout gh-pages
   if [[ $? == 0]];then
-    rm -rf ./
-    mv ./_book/* ./
+    rm -rf ./books ./gitbook
+    rm -f ./index.html ./methods.md ./search_index.json
+    mv ./_book/grep (index.\html|methods.\md|search_index.\json|books|gitbook) ./
     git status
     git add .
     git commit -m ":memo: update notes"
